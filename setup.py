46bd4e6910c707d7c70c68988e07af1ad8b4f7c8
Thomas A Caswell <tcaswell@gmail.com>
2015-06-19 23:59:34 -0400

    BLD: add setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='cycler',
+      version='0.1.0',
+      author='Thomas A Caswell',
+      py_modules=['cycler'],
+      description='Composable style cycles',
+      url='http://github.com/matplotlib/cycler',
+      platforms='Cross platform (Linux, Mac OSX, Windows)',
+      requires=['six'],
+      license="BSD"
+      )
