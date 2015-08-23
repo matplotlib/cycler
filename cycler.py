42975b752d4740157c5be6bef472b912a6da814d
Thomas A Caswell <tcaswell@gmail.com>
2015-08-22 21:23:22 -0400

    DOC: added docs for persistent style mapping

--- cycler.py
+++ cycler.py
@@ -377,7 +377,8 @@ class Cycler(object):
 
 
 def cycler(*args, **kwargs):
-    """
+    """Public constructor function for Cycler objects
+
     Create a new `Cycler` object from a single positional argument,
     a pair of positional arguments, or the combination of keyword arguments.
 
@@ -470,36 +471,36 @@ def _cycler(label, itr):
 
 
 class RememberTheStyle(Mapping):
-    def __init__(self, sty_cylr, loop=False, cache=None):
-        """Persistent style by key
+    """Persistent style by key
 
-        This is a class for easily managing consistent style across for
-        classes of data across many plots.
+    This is a class for easily managing consistent style across for
+    classes of data across many plots.
 
-        The first time you use `rts[key]` it gets the next style
-        dictionary from it's internal `Cycler`.  Subsequent calls to
-        `rts[key]` will return an identical dictionary.
+    The first time you use `rts[key]` it gets the next style
+    dictionary from it's internal `Cycler`.  Subsequent calls to
+    `rts[key]` will return an identical dictionary.
 
-        Parameters
-        ----------
-        sty_cylr : Cycler
+    Parameters
+    ----------
+    sty_cylr : Cycler
         Anything that yields dictionaries when iterated over
 
-        loop : bool, optional
-           Defaults to `False`.  If True, loop over the input `sty_cylr`
-           when more keys are requested that the length of `sty_cyclr`.
-           If `False` will raise `RuntimeError` if available styles are
-           exhausted.
+    loop : bool, optional
+        Defaults to `False`.  If True, loop over the input `sty_cylr`
+        when more keys are requested that the length of `sty_cyclr`.
+        If `False` will raise `RuntimeError` if available styles are
+        exhausted.
 
 
-        cache : dict-like or None, optional
-           MuttableMapping instance to use as the internal key -> style cache.
+    cache : dict-like or None, optional
+        MuttableMapping instance to use as the internal key -> style cache.
 
-           The purpose of this is provide a pre-populated cache or
-           to share a cache between multiple `RememberTheStyle` instances.
+        The purpose of this is provide a pre-populated cache or
+        to share a cache between multiple `RememberTheStyle` instances.
 
-           Use this at your own risk.
-        """
+        Use this at your own risk.
+    """
+    def __init__(self, sty_cylr, loop=False, cache=None):
         if cache is None:
             cache = {}
 
@@ -513,6 +514,11 @@ class RememberTheStyle(Mapping):
 
     @property
     def style_cache(self):
+        """Access to style cache instance
+
+        This is a reference to the underlying object, mutate
+        this with caution.
+        """
         return self._cache
 
     def __getitem__(self, key):
